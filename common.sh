35316d89045b83bfcc6d9e33eaa771546ee212f7
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2024-01-29 16:39:17 -0500

    a

--- common.sh
+++ common.sh
@@ -1391,6 +1391,10 @@ function createTenantYAMLCertManager_4_5_8() {
     kustomize build github.com/minio/operator/examples/kustomization/tenant-certmanager\?ref\=v4.5.8 > /Users/cniackz/bash-config/config-files/kustomize/Tenant/kustomize-tenant-cert-manager-4-5-8.yaml
 }
 
+function createTenantYAML_4_5_8() {
+	kustomize build github.com/minio/operator/examples/kustomization/tenant-lite\?ref\=v4.5.8 > /Users/cniackz/bash-config/config-files/kustomize/Tenant/kustomize-tenant-4-5-8.yaml
+}
+
 function createTenantYAMLCertManager_5_0_4() {
     kustomize build github.com/minio/operator/examples/kustomization/tenant-certmanager\?ref\=v5.0.4 > /Users/cniackz/bash-config/config-files/kustomize/Tenant/kustomize-tenant-cert-manager-5-0-4.yaml
 }
