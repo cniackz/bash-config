b6103867a33c9f3deb44261c44d074bb11495259
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2024-06-09 13:06:40 -0400

    Saving time by not running krew commands every time

--- common.sh
+++ common.sh
@@ -2,22 +2,6 @@
 
 ######################################
 #
-# Krew
-# https://krew.sigs.k8s.io/docs/user-guide/setup/install/
-#
-######################################
-(
-  set -x; cd "$(mktemp -d)" &&
-  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-  KREW="krew-${OS}_${ARCH}" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "${KREW}.tar.gz" &&
-  ./"${KREW}" install krew
-)
-
-######################################
-#
 # Environment Variables
 #
 ######################################
@@ -2998,6 +2982,74 @@ mc ls myminio/bucket --insecure
 # Es el remove pool el que causa la perdida de datos?
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################################
+#
+# Krew
+# https://krew.sigs.k8s.io/docs/user-guide/setup/install/
+#
+######################################
+# NOTE: Don't run this on every new window, instead only when needed.
+# Reason, this is time consuming and not always needed, so will save you time based on the experience.
+function krew() {
+    (
+      set -x; cd "$(mktemp -d)" &&
+      OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+      ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+      KREW="krew-${OS}_${ARCH}" &&
+      curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+      tar zxvf "${KREW}.tar.gz" &&
+      ./"${KREW}" install krew
+    )
+}
 
 
 
